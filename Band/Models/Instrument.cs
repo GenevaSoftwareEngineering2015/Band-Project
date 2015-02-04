20d9506e05b1d51c13feee795e9b254ef2b84fa2
Matt Kennedy <Matt.Kenn3dy@gmail.com>
2015-02-03 21:41:23 -0500

    Band Check IN
    Hopefully This Works

---
+++ Band/Models/Instrument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Band.Models
+{
+    public class Instrument
+    {
+        public string InstrumentID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
