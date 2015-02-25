dad6137317c053edb52c27ac35c419f17781e58a
dre3520 <dre3520@gmail.com>
2015-02-25 11:53:39 -0500

    Added xunit class library and xunit 1.9.2 package with two basic tests to see if AppVeyor automatically finds it

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            Assert.Equal(5, 5);
+        }
+    }
+}
