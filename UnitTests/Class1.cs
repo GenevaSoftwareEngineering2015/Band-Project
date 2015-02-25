5407556471aa0c14ee865bc4533813b23b40c402
dre3520 <dre3520@gmail.com>
2015-02-25 13:14:40 -0500

    Added reference to Band project in the UnitTests project and made 1 test to interact with CheckOutsController

--- UnitTests/Class1.cs
+++ UnitTests/Class1.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using Xunit;
+using Band.Controllers;
+
 namespace UnitTests
 {
     public class Class1
     {
         [Fact]
-        public void PassingTest()
+        public void FirstRealTest()
         {
-            Assert.Equal(4, 4);
-        }
-
-        [Fact]
-        public void TestTwo()
-        {
-            Assert.Equal(5, 5);
+            var controller = new CheckOutsController();
+            var result = controller.Index() as ViewResult;
+            Assert.Equal("Index", result.ViewName);
         }
     }
 }
