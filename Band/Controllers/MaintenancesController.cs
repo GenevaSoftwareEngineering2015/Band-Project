ac73b811d028cb2cccff143b8b878a7be2db34bc
dre3520 <dre3520@gmail.com>
2015-03-03 19:08:40 -0500

    Got maintainance page up to date

--- Band/Controllers/MaintenancesController.cs
+++ Band/Controllers/MaintenancesController.cs
@@ -46,7 +46,7 @@ namespace Band.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "MaintenanceID,InstrumentName,MComment,MDate")] Maintenance maintenance)
+        public ActionResult Create([Bind(Include = "InstrumentName,MComment,MDate")] Maintenance maintenance)
         {
             if (ModelState.IsValid)
             {
