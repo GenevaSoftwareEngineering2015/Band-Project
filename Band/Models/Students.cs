71e9551cf43748f023a41287a1c8cfe32146503e
Matt Kennedy <Matt.Kenn3dy@gmail.com>
2015-04-21 19:36:56 -0400

    Added Student
    Student Added

---
+++ Band/Models/Students.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Band.Models
+{
+    public class Students
+    {
+        [Key]
+        public int id { get; set; }
+        [Display(Name = "Student Name")]
+        public string StudName { get; set; }
+        [Display(Name = "Student Graduation Year")]
+        public string StudGradYear { get; set; }
+        [Display(Name = "Instrument Type")]
+        public string StudInstType { get; set; }
+        [Display(Name = "Instrument Name")]
+        public string StudInstrument { get; set; }
+    }
+}
