4c83b50ecbab2f028401e4994bd27b367c675544
unrelatedname193 <unrelatedname193@github.com>
2015-05-05 16:50:31 -0400

    Selenium framework integrated; does not compile; result of multiple errors

---
+++ Band/SeleniumTests/Chromedrivertest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Band.SeleniumTests
+{
+    class Chromedrivertests
+    {
+        static void main()
+        {
+           IWebDriver driver = new ChromeDriver();
+
+           driver.Navigate().GoToUrl("localhost");
+
+           driver.time.sleep(2000);
+
+           driver.Navigate().GoToUrl("localhost/register");
+
+           driver.time.sleep(2000);
+        }
+    }
+}
